5946b3f0c437f32d7a681617179cf4d0aeae57f1
cauexmx <127966350+cauexmx@users.noreply.github.com>
2023-05-19 11:50:37 -0300

    Add files via upload

---
+++ promp.py
@@ -0,0 +1,40 @@
+print("Mensagem que eu quero")
+
+print("Mensagem 2")
+
+print('Fulando é meu "amigo"')
+
+print("Fulano é meu \"amigo\" ")
+
+print(1+1)
+
+print("1 x 1 =", 1*1)
+
+print("10² =", 10**3)
+
+print(pow(10,3))
+
+
+nome ="A"
+
+x = 1
+x = 1+1
+y = True
+print(y)
+
+nome = str("")
+
+nome = 1+1
+
+x = "A"
+print(x)
+
+x = 5.3
+print(x)
+
+x1 = "5"
+print("5.3 x 5")
+
+print(float(x) * int(x1))
+y = True
+print(y)
